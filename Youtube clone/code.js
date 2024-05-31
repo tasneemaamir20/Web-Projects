2287eeb3342f1dcb5b003c792a2ff5240d4f66f6
Tasneem Aamir <117052484+tasneemaamir20@users.noreply.github.com>
2024-05-31 23:07:23 +0530

    Add files via upload

---
+++ Youtube clone/code.js
@@ -0,0 +1,31 @@
+async function youtube()
+{
+    let data =await fetch("./data.json");
+    let val=await data.json();
+    console.log(val);
+
+
+    let playbox=document.getElementById("playbox");
+
+
+    let playlist=document.getElementById("playlist");
+
+
+
+    for(let i of val)
+    {
+        let div=document.createElement("div");
+        div.innerHTML=`
+        <img src="${i.thumbnail}" height="100px" width="200px">
+        <h1> ${i.title}</h1>
+        <br><br>`
+        playlist.appendChild(div);
+
+        div.addEventListener("click",()=>
+        {
+            playbox.innerHTML=`
+            <video src="${i.video}" controls autoplay></video>`
+        })
+    }
+}
+youtube();
